4feffe593d8b60bf17fb021976b86daa42c8abd0
no-glue <nikola_pav@eunet.rs>
2013-09-19 22:11:16 +0200

    softmax test

---
+++ Softmax.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace bandits;
+
+class Softmax{
+	public function __construct(
+		$temperature=0.3,
+		$counts=array(),
+		$values=array()
+	){
+		$this->temperature=$temperature;
+		$this->counts=$counts;
+		$this->values=$values;
+	}
+
+	public function initialize($narms){
+		$this->counts=array_pad(
+			$this->counts,
+			$narms,
+			0
+		);
+
+		$this->values=array_pad(
+			$this->values,
+			$narms,
+			0
+		);
+	}
+
+	public function selectArm(){
+		$sum=exp(
+			array_sum(
+				$this->values
+			)
+			/
+			$this->temperature
+		);
+
+		$probabilities=array();
+
+		foreach($this->values as $value){
+			$probabilities[]=exp(
+				$value
+				/
+				$this->temperature
+			)
+			/
+			$sum;
+		}
+
+		$random=mt_rand();
+
+		$cumulativeProbability=0;
+
+		$i=0;
+
+		while(
+			($cumulativeProbability+=array_shift($probabilities))
+			<=
+			$random && ++$i
+		){
+		}
+
+		return $i;
+	}
+
+	public function update(
+		$chosenArm,
+		$reward
+	){
+		$this->counts[$chosenArm]+=1;
+		
+		$n=$this->counts[$chosenArm];
+
+		$value=$this->values[$chosenArm];
+
+		$newValue=(($n-1)/(float)$n)*$value+
+			(1/(float)$n)*$reward;
+
+		$this->values[$chosenArm]=$newValue;	
+	}
+}
